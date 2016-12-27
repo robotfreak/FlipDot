034385b42d9eded8d87dcb751e3ded4bb0c13cfa
RobotFreak <p.recktenwald@gmail.com>
2016-12-27 16:27:06 +0100

    GameOfLife: reduce used memory.

--- Arduino/FlipdotSimulation/gameoflife.c
+++ Arduino/FlipdotSimulation/gameoflife.c
@@ -40,7 +40,7 @@
 //================ global Variables ==========================
 // frameBuffer stores the content of the Flipdotmatrix
 // Each pixel is one bit (I.e only an eigth of storage is required
-unsigned char frameBuffer[X_SIZE][Y_SIZE];
+uint8_t frameBuffer[X_SIZE][Y_SIZE];
 
 //===========================================
 // clearFrameBuffer(int color)
@@ -105,6 +105,7 @@ int getFrameBuffer(int x, int y)
   }
   return value;
 }
+
 
 //============================================
 // printCharXxY(int xOffs, int yOffs, char c)
@@ -313,8 +314,57 @@ void printFrameBuffer()
 #define NUMCOLS X_SIZE
 
 //bool gameBoard[NUMROWS][NUMCOLS];
-bool newGameBoard[NUMROWS][NUMCOLS];
+uint8_t newGameBoard[NUMCOLS][NUMROWS/8];
 
+//===========================================
+// setNewGameBoard(int x, int y, int value)
+// Set one Pixel at x,y-Position
+// value can be ON or OFF
+//===========================================
+void setNewGameBoard(int x, int y, int value)
+{
+  unsigned char w, wNot;
+  int yByteNo, yBitNo;
+
+  w = 1;
+  if ((y < NUMROWS) && (x < NUMCOLS) && (x >= 0) && (y >= 0))
+  {
+    yByteNo = y / 8; // integer division to select the byte
+    yBitNo = y % 8;  // module division (residual) to select the bit in that byte
+    w = w << yBitNo;
+    if (value == ON)
+    {
+      newGameBoard[x][yByteNo] = newGameBoard[x][yByteNo] | w; // Logical OR adds one bit to the existing byte
+    }
+    else
+    {
+      wNot = 0xFF - w;
+      newGameBoard[x][yByteNo] = newGameBoard[x][yByteNo] & wNot; // Logical AND set one bit to zero in the existing byte
+    }
+  }
+}
+
+//===========================================
+// getNewFrameBuffer(int x, int y)
+// Get one Pixel at x,y-Position
+// returns value can be ON or OFF
+//===========================================
+int getNewGameBoard(int x, int y)
+{
+  unsigned char w, wNot;
+  int yByteNo, yBitNo;
+  int value = 0;
+
+  w = 1;
+  if ((y < NUMROWS) && (x < NUMCOLS) && (x >= 0) && (y >= 0))
+  {
+    yByteNo = y / 8; // integer division to select the byte
+    yBitNo = y % 8;  // module division (residual) to select the bit in that byte
+    w = w << yBitNo;
+    if (newGameBoard[x][yByteNo] & w) value = 1; else value = 0;
+  }
+  return value;
+}
 
 //===========================================
 // randomInt(int min_num, int max_num)
@@ -378,11 +428,11 @@ int initGameBoard(int xOffs, int yOffs, int xSize, int ySize, const char *s)
 }
 
 //===========================================
-// isCellAlive(uint8_t row, uint8_t col)
+// isCellAlive(int row, int col)
 // Returns whether or not the specified cell is on.
 // If the cell specified is outside the game board, returns false.
 //===========================================
-bool isCellAlive(uint8_t row, uint8_t col)
+bool isCellAlive(int row, int col)
 {
   if (row < 0 || col < 0 || row >= NUMROWS || col >= NUMCOLS)
   {
@@ -392,14 +442,14 @@ bool isCellAlive(uint8_t row, uint8_t col)
 }
 
 //===========================================
-// countNeighbors(uint8_t row, uint8_t col)
+// countNeighbors(int row, int col)
 // Counts the number of active cells surrounding the specified cell.
 // Cells outside the board are considered "off"
 // Returns a number in the range of 0 <= n < 9
 //===========================================
-uint8_t countNeighbors(uint8_t row, uint8_t col)
+int countNeighbors(int row, int col)
 {
-  uint8_t count = 0;
+  int count = 0;
   char rowDelta, colDelta;
   for (rowDelta = -1; rowDelta <= 1; rowDelta++)
   {
@@ -427,7 +477,7 @@ uint8_t countNeighbors(uint8_t row, uint8_t col)
 //===========================================
 void calculateNewGameBoard()
 {
-  uint8_t row, col, numNeighbors;
+  int row, col, numNeighbors;
   for (row = 0; row < NUMROWS; row++)
   {
     for (col = 0; col < NUMCOLS; col++)
@@ -436,27 +486,27 @@ void calculateNewGameBoard()
       if (getFrameBuffer(col,row) && numNeighbors < 2)
       {
         // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-        newGameBoard[row][col] = false;
+        setNewGameBoard(col, row, OFF); // newGameBoard[col][row] = false;
       }
       else if (getFrameBuffer(col,row) && (numNeighbors == 2 || numNeighbors == 3))
       {
         // Any live cell with two or three live neighbours lives on to the next generation.
-        newGameBoard[row][col] = true;
+         setNewGameBoard(col, row, ON); // newGameBoard[col][row] = true;
       }
       else if (getFrameBuffer(col,row) && numNeighbors > 3)
       {
         // Any live cell with more than three live neighbours dies, as if by overcrowding.
-        newGameBoard[row][col] = false;
+         setNewGameBoard(col, row, OFF); // newGameBoard[col][row] = false;
       }
       else if (!getFrameBuffer(col,row) && numNeighbors == 3)
       {
         // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-        newGameBoard[row][col] = true;
+         setNewGameBoard(col, row, ON); // newGameBoard[col][row] = true;
       }
       else
       {
         // All other cells will remain off
-        newGameBoard[row][col] = false;
+         setNewGameBoard(col, row, OFF); // newGameBoard[col][row] = false;
       }
     }
   }
@@ -469,12 +519,13 @@ void calculateNewGameBoard()
 //===========================================
 void swapGameBoards()
 {
-  uint8_t row, col;
+  int row, col, val;
   for (row = 0; row < NUMROWS; row++)
   {
     for (col = 0; col < NUMCOLS; col++)
     {
-      setFrameBuffer(col, row, newGameBoard[row][col]);
+      val = getNewGameBoard(col, row);
+      setFrameBuffer(col, row, val);
     }
   }
 }
@@ -509,7 +560,7 @@ int main(int argc, char *argv[])
   //initGameBoard(24, 2, 8, 4, "02C04700");   // die hard
   //initGameBoard(20, 8, 8, 6, "020b0a0820a0");   // infinity
   //initGameBoard(20, 8, 5, 5, "1D10030D15");   // infinity2
-  initGameBoard(20, 8, 40, 1, "7FBE381FDF");   // infinity3
+  initGameBoard(5, 8, 40, 1, "7FBE381FDF");   // infinity3
   //initGameBoard();
   printFrameBuffer();
   sleep(1);
