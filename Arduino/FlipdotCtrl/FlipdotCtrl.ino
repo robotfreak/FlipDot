b6841cddea4e6b31d1afffad9f232c67b2739df3
RobotFreak <p.recktenwald@gmail.com>
2017-03-08 22:52:20 +0100

    added RTC lib for time control

--- Arduino/FlipdotCtrl/FlipdotCtrl.ino
+++ Arduino/FlipdotCtrl/FlipdotCtrl.ino
@@ -1,3 +1,4 @@
+
 ////////////////////////////////////////////////////////////////////////////
 // Flipdot_Firmware
 // CC-BY-SA-NC    8/2016 C-Hack (Ralf@Surasto.de)
@@ -38,19 +39,34 @@
 //     It gets evaluated after reception of that character
 //
 ////////////////////////////////////////////////////////////////////////////
+#include <Wire.h>
+#include <TimeLib.h>
+#include <RTClib.h>
 
 #include "Flipdot.h"
 
 int i, j;
 int inByte;
 String commandLine;
+unsigned long previousMillis = 0;        // will store last time from update
+int fdState = 0;
+int fdMode = 0;
+
 
 FlipDot flipdot(FD_COLUMS, FD_ROWS);
 
+RTC_DS1307 rtc;
+DateTime tm;
+unsigned long current_time;
+unsigned long last_time;
+int hours, minutes; //, day, month, year;
+
+//char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup() {
 
   Serial.begin(115200);
+  Serial.println("FlipdotControl v1.0");
   flipdot.begin();
   flipdot.update();
   delay(1000);
@@ -59,10 +75,27 @@ void setup() {
   flipdot.setLedColor(0, 0, 0xFF);
   delay(1000);
 
+  if (! rtc.begin()) {
+  }
+  else if (! rtc.isrunning()) {
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+
+
   flipdot.setLedColor(0, 0xFF, 0);
   //
   //  while(1)
   //    printHello();
+
+  clearFrameBuffer(OFF);
+  i = printString(5, 0, ON, XLARGE, "IN-BERLIN");
+  flipdot.update();
+
 }
 
 void loop() {
@@ -79,6 +112,8 @@ void loop() {
   String xStr, yStr, str;
   String xSizStr, ySizStr;
   String outputString;
+
+
 
   if (Serial.available() > 0) {
     c = Serial.read();
@@ -209,133 +244,297 @@ void loop() {
       }
       Serial.println(outputString);
 
+      fdMode = 0;
+      fdState = 0;
       // ======= Execute the respective command ========
       switch (cmd) {
         case 'C':  clearFrameBuffer(color); Serial.println("C"); updatePanel(); break;
         case 'L':  flipdot.setLedColor(r, g, b); Serial.println("L"); break;
-        case 'T':  printTest(yVal); Serial.println("T"); break;
         case 'S':  setPixel(xVal, yVal, color); break;
         case 'H':  hLine(yVal, color); updatePanel(); Serial.println("H"); break;
         case 'V':  vLine(xVal, color); updatePanel(); Serial.println("V"); break;
         case 'P':  printString(xVal, yVal, color, fsize, outputString); updatePanel(); Serial.println("P");  break;
         case 'B':  printBitmap(xVal, yVal, color, xSiz, ySiz, outputString); updatePanel(); Serial.println("B"); break;
         case 'U':  updatePanel(); Serial.println("U"); break;
+        case 'f':  fdMode = 1; break;
+        case 'n':  fdMode = 2; break;
+        case 't':  fdMode = 3; break;
+        case 'd':  fdMode = 4; break;
+          // case 'h':  showHelp(); break;
       }
     }
+  }
+
+  tm = rtc.now();
+  hours = tm.hour();
+  minutes = tm.minute();
+  if (hours > 5 && hours < 19)
+  {
+    printNews();
+  }
+  if (hours < 3 || hours > 16)
+  {
+    flipdot.setLedColor(0, 0xFF, 0);
+
+  }
+  else
+  {
+    flipdot.setLedColor(0, 0, 0);
+
+  }
+
+
+  switch (fdMode) {
+    case 1:
+      flipTest();
+      break;
+    case 2:
+      printNews();
+      break;
+    case 3:
+      printTest();
+      break;
+    case 4:
+      printDateTime();
+      break;
+    default:
+      break;
+  }
+}
+
+#if 0
+void showHelp(void)
+{
+  Serial.println("Flipdot Control v1.1");
+  Serial.println("-----------------------");
+  Serial.println("Command format");
+  Serial.println("<Command>,<Color>,<x>,<y>,<size>,[<xsize>,<ysize>]<....string....>\\\n");
+  //
+  Serial.println("Commands:");
+  Serial.println("  C  Clear Screen");
+  Serial.println("  B  Draw a Bitmap");
+  Serial.println("  L  RGB LED");
+  Serial.println("  P  Print Text");
+  Serial.println("  H  Draw a horizontal line");
+  Serial.println("  V  Draw a vertical line");
+  Serial.println("  S  Set a pixel");
+  Serial.println("  U  Update Panel");
+  Serial.println("Color:");
+  Serial.println("  B  Black");
+  Serial.println("  Y  Yellow");
+  Serial.println("X,Y:");
+  Serial.println("  Required for all Print commands");
+  Serial.println("  Only Y is required for the horizontal line command \"H\"");
+  Serial.println("  Only X is required for the vertical line command \"V\"");
+  Serial.println("size:");
+  Serial.println("  Not required for Bitmap commands");
+  Serial.println("  S SMALL");
+  Serial.println("  M MEDIUM");
+  Serial.println("  L LARGE");
+  Serial.println("  X EXTRALARGE");
+  Serial.println("XSize,YSize:");
+  Serial.println("  Required only for Bitmap commands");
+  Serial.println("String:");
+  Serial.println("  Contains the characters to be printed");
+  Serial.println("\\n");
+  Serial.println("  The command lines is terminated by the \\ character");
+  Serial.println("  It gets evaluated after reception of that character");
+}
+#endif
+
+String getTimeStr(void) {
+  String str = "";
+  tm = rtc.now();
+  hours = tm.hour();
+  minutes = tm.minute();
+  print2digits(hours);
+  Serial.write(':');
+  print2digits(minutes);
+  Serial.println("\n");
+  if (hours < 10)
+    str += "0";
+  str += String(hours);
+  str += ":";
+  if (minutes < 10)
+    str += "0";
+  str += String(minutes);
+  return str;
+}
+
+String getDateStr(void) {
+  String str = "";
+  //  tm = rtc.now();
+  //  day = tm.day();
+  //  year = tm.year();
+  //  month = tm.month();
+  //  if (day < 10)
+  //    str += "0";
+  str += String(day());
+  str += ".";
+  //  if (month < 10)
+  //    str += "0";
+  str += String(month());
+  str += ".";
+  str += String(year());
+  return str;
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
+}
+//===================================
+// For debugging and testing only
+//===================================
+void printNews() {
+
+  unsigned long currentMillis = millis();
+  String str;
+
+  if (currentMillis - previousMillis >= 30000) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    clearFrameBuffer(OFF);
+    switch (fdState)
+    {
+      case 0:
+        i = printString(5, 0, ON, XLARGE, "IN-BERLIN");
+        fdState = 1;
+        break;
+      case 1:
+        i = printString(5, 0, ON, XSMALL, "Di 19-22");
+        i = printString(5, 8, ON, XSMALL, "Fr 19-01");
+        i = printString(60, 0, ON, XLARGE, "ELAB");
+        fdState = 0;
+        break;
+      default:
+        fdState = 0;
+        break;
+
+    }
+    updatePanel();
+  }
+
+}
+
+//===================================
+// For debugging and testing only
+//===================================
+void printTest() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= 10000) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    clearFrameBuffer(OFF);
+    switch (fdState)
+    {
+      case 0:
+        i = printString(1, 0, ON, XSMALL, "ABCDEFGHIJKLM");
+        i = printString(1, 6, ON, XSMALL, "NOPQRSTUVWXYZ");
+        i = printString(1, 11, ON, XSMALL, "1234567890()[]");
+        Serial.println("Extra Small Font 3x5  ");
+        fdState++;
+        break;
+      case 1:
+        i = printString(1, 0, ON, SMALL, "ABCDEFGHIJKLM");
+        i = printString(1, 8, ON, SMALL, "NOPQRSTUVWXYZ");
+        Serial.println("Small Font 6x8       ");
+        fdState++;
+        break;
+      case 2:
+        i = printString(1, 0, ON, MEDIUM, "ABCDEFGHIJKLM");
+        i = printString(1, 8, ON, MEDIUM, "NOPQRSTUVWXYZ");
+        Serial.println("Medium Font 8x8      ");
+        fdState++;
+        break;
+      case 3:
+        i = printString(2, 2, ON, LARGE, "ABCDEFGHIJKLM");
+        Serial.println("Large Font 8x12      ");
+        fdState++;
+        break;
+      case 4:
+        i = printString(2, 0, ON, XLARGE, "ABCDEF");
+        Serial.println("Extra Large Font 9x16 ");
+        fdState++;
+        break;
+      case 5:
+        i = printBitmap(2, 0, ON, 4, 4, "09000906");
+        i = printBitmap(2, 6, ON, 4, 4, "09000609");
+        i = printBitmap(2, 12, ON, 4, 4, "09000f00");
+        i = printBitmap(12, 0, ON, 8, 8, "0066660081423C00");
+        i = printBitmap(12, 8, ON, 8, 8, "006666003C428100");
+        i = printBitmap(22, 2, ON, 5, 5, "0A1F1F0E04");
+        i = printBitmap(32, 2, ON, 8, 9, "FF81422418244281FF");
+        Serial.println("Bitmap Grafik");
+        fdState++;
+        break;
+      default:
+        fdState = 0;
+        break;
+    }
+    updatePanel();
   }
 }
 
 //===================================
 // For debugging and testing only
 //===================================
-void printHello() {
+void printDateTime(void) {
 
-  clearFrameBuffer(OFF);
-  i = printString(5, 0, ON, XLARGE, "IN-BERLIN");
-  updatePanel();
-  delay(30000);
+  unsigned long currentMillis = millis();
 
-  clearFrameBuffer(OFF);
-  i = printString(5, 0, ON, XSMALL, "Di 19-22");
-  i = printString(5, 8, ON, XSMALL, "Fr 19-01");
-  i = printString(60, 0, ON, XLARGE, "ELAB");
-  updatePanel();
-  delay(30000);
+  if (currentMillis - previousMillis >= 10000) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    switch (fdState)
+    {
+      case 0:
+        getTimeStr();
+        fdState = 1;
+        break;
+      case 1:
+        getDateStr();
+        fdState = 0;
+        break;
+      default:
+        fdState = 0;
+        break;
 
+    }
+    updatePanel();
+  }
 }
 
-//===================================
-// For debugging and testing only
-//===================================
-void printTest(int y) {
-
-  clearFrameBuffer(OFF);
-  i = printString(1, 0, ON, XSMALL, "ABCDEFGHIJKLM");
-  i = printString(1, 6, ON, XSMALL, "NOPQRSTUVWXYZ");
-  i = printString(1, 11, ON, XSMALL, "1234567890()[]");
-  updatePanel();
-  Serial.println("Extra Small Font 3x5  ");
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  i = printString(1, 0, ON, SMALL, "ABCDEFGHIJKLM");
-  i = printString(1, 8, ON, SMALL, "NOPQRSTUVWXYZ");
-  updatePanel();
-  Serial.println("Small Font 6x8       ");
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  i = printString(1, 0, ON, SMALL, "abcdefghijklm");
-  i = printString(1, 8, ON, SMALL, "nopqrstuvwxyz");
-  updatePanel();
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  i = printString(1, 0, ON, MEDIUM, "ABCDEFGHIJKLM");
-  i = printString(1, 8, ON, MEDIUM, "NOPQRSTUVWXYZ");
-  updatePanel();
-  Serial.println("Medium Font 8x8      ");
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  i = printString(1, 0, ON, MEDIUM, "abcdefghijklm");
-  i = printString(1, 8, ON, MEDIUM, "nopqrstuvwxyz");
-  updatePanel();
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  i = printString(2, 2, ON, LARGE, "ABCDEFGHIJKLM");
-  Serial.println("Large Font 8x12      ");
-  updatePanel();
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  i = printString(2, 2, ON, LARGE, "abcdefghijklm");
-  updatePanel();
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  i = printString(2, 0, ON, XLARGE, "ABCDEF");
-  updatePanel();
-  Serial.println("Extra Large Font 9x16 ");
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  i = printString(2, 0, ON, XLARGE, "123456");
-  updatePanel();
-  delay(2000);
-
-  clearFrameBuffer(OFF);
-  //i =   printBitmap(0,0,ON,8,8,"1020408001020408");
-  i = printBitmap(2, 0, ON, 4, 4, "09000906");
-  i = printBitmap(2, 6, ON, 4, 4, "09000609");
-  i = printBitmap(2, 12, ON, 4, 4, "09000f00");
-  i = printBitmap(12, 0, ON, 8, 8, "0066660081423C00");
-  i = printBitmap(12, 8, ON, 8, 8, "006666003C428100");
-  i = printBitmap(22, 2, ON, 5, 5, "0A1F1F0E04");
-  i = printBitmap(32, 2, ON, 8, 9, "FF81422418244281FF");
-  //i = printBitmap(42, 0, ON, 16, 16, "1000010000100001111122224444888811112222444488881111222244448888");
-  //i = printBitmap(60, 0, ON, 16, 16, "FFFF800180018001800180018001FFFF8001800180018001800180018001FFFF");
-  //   vLine(0,ON);
-  //   vLine(77,ON);
-  //   hLine(0,ON);
-  //   hLine(15,ON);
-  updatePanel();
-  Serial.println("Bitmap Grafik");
-  delay(2000);
-}
 
 //===================================
 // For debugging and testing only
 //===================================
 void flipTest(void) {
 
-  clearFrameBuffer(OFF);
-  updatePanel();
-  delay(1000);
+  unsigned long currentMillis = millis();
 
-  clearFrameBuffer(ON);
-  updatePanel();
-  delay(1000);
+  if (currentMillis - previousMillis >= 10000) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    switch (fdState)
+    {
+      case 0:
+        clearFrameBuffer(OFF);
+        fdState = 1;
+        break;
+      case 1:
+        clearFrameBuffer(ON);
+        fdState = 0;
+        break;
+      default:
+        fdState = 0;
+        break;
+
+    }
+    updatePanel();
+  }
 }
 
